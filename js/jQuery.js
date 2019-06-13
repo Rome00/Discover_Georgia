9e679ae3ba881bd2b2846b69ba66d361805fe49b
Rome00 <romeo.khazalia@geolab.edu.ge>
2019-06-13 16:17:28 +0400

    final

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,13 +1,15 @@
-var links = $(".in-link");
+var links = $(".nav-link");
+var nav = $("header .navbar-nav");
+var navBurger = $("header .navbar-toggler");
+var windowPage = $("window");
 $(document).ready(function () {
-    // Smooth scrolling
-    links.click(function (e) {
-        e.preventDefault();
-        $("body,html").animate(
-            {
-                scrollTop: $(this.hash).offset().top 
-            },
-            1000
-        );
+    // hide
+    links.click(function () {
+        nav.removeClass("open-md-nav");
+        navBurger.removeClass("Burgeractive");
     });
 });
+$(window).scroll(function () {
+    nav.removeClass("open-md-nav");
+    navBurger.removeClass("Burgeractive");
+})
